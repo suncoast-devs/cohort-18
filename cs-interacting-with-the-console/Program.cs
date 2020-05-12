451becead06f9054bf163782dcfb06f7f2cb6093
Gavin Stark <gavin@gstark.com>
2020-05-12 14:18:12 -0400

    Interacting with the console

---
+++ cs-interacting-with-the-console/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace InteractingWithTheConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your name? ");
+            // Reading a string INTO a variable
+            var name = Console.ReadLine();
+
+            var greeting = $"It is a pleasure to meet you, {name}. I hope you, {name}, enjoy my program.";
+            // Writing a string into the console
+            Console.WriteLine(greeting);
+
+            Console.Write("What is your favorite number? ");
+            var favoriteNumberString = Console.ReadLine();
+
+            // Convert string into integer
+            int favoriteNumber = int.Parse(favoriteNumberString);
+
+            var doubleYourNumber = $"Your favorite number times two is {favoriteNumber * 2}";
+            Console.WriteLine(doubleYourNumber);
+        }
+    }
+}
---
+++ cs-interacting-with-the-console/README.md
@@ -0,0 +1,1 @@
+# cs_interacting_with_the_console
