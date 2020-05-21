617fae0e3f26ef18b3f6f470cf867ea1110ca520
Gavin Stark <gavin@gstark.com>
2020-05-21 10:21:04 -0400

    Initial version of NumberTracker

---
+++ NumberTracker/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NumberTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Number Tracker");
+
+            // Creates a list of numbers we will be tracking
+            var numbers = new List<int>();
+
+            // Controls if we are still running our loop asking for more numbers
+            var isRunning = true;
+
+            // While we are running
+            while (isRunning)
+            {
+                // Show the list of numbers
+                Console.WriteLine("------------------");
+                foreach (var number in numbers)
+                {
+                    Console.WriteLine(number);
+                }
+                Console.WriteLine("------------------");
+
+                // Ask for a new number or the word quit to end
+                Console.Write("Enter a number to store, or 'quit' to end: ");
+                var input = Console.ReadLine().ToLower();
+
+                if (input == "quit")
+                {
+                    // If the input is quit, turn off the flag to keep looping
+                    isRunning = false;
+                }
+                else
+                {
+                    // Parse the number and add it to the list of numbers
+                    var number = int.Parse(input);
+                    numbers.Add(number);
+                }
+            }
+        }
+    }
+}
