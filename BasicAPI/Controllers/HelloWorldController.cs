b2d10867d3e982d5347e791e663a4f631f758f56
Gavin Stark <gavin@gstark.com>
2020-06-03 10:54:45 -0400

    Now our HelloWorld supports a query parameter we use in the greeting

--- BasicAPI/Controllers/HelloWorldController.cs
+++ BasicAPI/Controllers/HelloWorldController.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BasicAPI.Controllers
@@ -16,10 +17,29 @@ namespace BasicAPI.Controllers
         // If you do GET https://localhost:5001/HelloWorld
         // you'll get *THIS* code.
         [HttpGet]
-        public string SayHello()
+        public string SayHello(string who)
         {
             // This is the response
-            return "Hello, World.";
+            var theTimeRightNow = DateTime.Now;
+
+            string whoOrWorld;
+
+            // If there wasn't a "who" query parameter
+            if (who == null)
+            {
+                // Use this as our greeting
+                whoOrWorld = "World";
+            }
+            else
+            {
+                // Otherwise, use the query parameter
+                whoOrWorld = who;
+            }
+
+            // This is a one-liner of the above
+            // string whoOrWorld = who == null ? "World" : who;
+
+            return $"Hello, {whoOrWorld}. It is currently {theTimeRightNow}. Have a great day.";
         }
     }
 }
