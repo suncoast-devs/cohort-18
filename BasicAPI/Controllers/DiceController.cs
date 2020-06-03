4ba24b7584ddcc9e062b163d188371f20cec9555
Gavin Stark <gavin@gstark.com>
2020-06-03 12:10:01 -0400

    Can now roll multiple dice

--- BasicAPI/Controllers/DiceController.cs
+++ BasicAPI/Controllers/DiceController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BasicAPI.Controllers
@@ -14,20 +15,36 @@ namespace BasicAPI.Controllers
         //        |
         //        v
         [HttpGet("{sides}")]
+        //
         // Examples:
         //    GET /Dice/6       <= sides = 6
         //    GET /Dice/20      <= sides = 20
-        public int Roll(int sides)
+        //
+        public List<int> Roll(int sides, int count = 1)
+        //                    ^          ^
+        //                    url param  query param
+        //
         {
             var randomNumberGenerator = new Random();
 
-            // generate a random number between 1 and sides (inclusive)
-            // example: between 1 and 6
-            // example: between 1 and 20
+            // Build a list of integers
+            var rolls = new List<int>();
 
-            var roll = randomNumberGenerator.Next(sides) + 1;
+            // Do this loop *count* times.
+            for (var rollNumber = 0; rollNumber < count; rollNumber++)
+            {
+                // generate a random number between 1 and sides (inclusive)
+                // example: between 1 and 6
+                // example: between 1 and 20
 
-            return roll;
+                var roll = randomNumberGenerator.Next(sides) + 1;
+
+                // Add that roll to our list
+                rolls.Add(roll);
+            }
+
+            // We return lists of rolls
+            return rolls;
         }
     }
 }
--- BasicAPI/Controllers/HelloWorldController.cs
+++ BasicAPI/Controllers/HelloWorldController.cs
@@ -17,29 +17,12 @@ namespace BasicAPI.Controllers
         // If you do GET https://localhost:5001/HelloWorld
         // you'll get *THIS* code.
         [HttpGet]
-        public string SayHello(string who)
+        public string SayHello(string who = "World")
         {
             // This is the response
             var theTimeRightNow = DateTime.Now;
 
-            string whoOrWorld;
-
-            // If there wasn't a "who" query parameter
-            if (who == null)
-            {
-                // Use this as our greeting
-                whoOrWorld = "World";
-            }
-            else
-            {
-                // Otherwise, use the query parameter
-                whoOrWorld = who;
-            }
-
-            // This is a one-liner of the above
-            // string whoOrWorld = who == null ? "World" : who;
-
-            return $"Hello, {whoOrWorld}. It is currently {theTimeRightNow}. Have a great day.";
+            return $"Hello, {who}. It is currently {theTimeRightNow}. Have a great day.";
         }
     }
 }
