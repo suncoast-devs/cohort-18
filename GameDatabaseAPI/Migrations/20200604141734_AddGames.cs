eb9ce5d6d5aa941f6011e6beaef83b0637be8b64
Gavin Stark <gavin@gstark.com>
2020-06-04 10:31:20 -0400

    Adds migrations for Games

---
+++ GameDatabaseAPI/Migrations/20200604141734_AddGames.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace GameDatabaseAPI.Migrations
+{
+    public partial class AddGames : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Host = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true),
+                    When = table.Column<DateTime>(nullable: false),
+                    MinimumPlayers = table.Column<int>(nullable: false),
+                    MaximumPlayers = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Games");
+        }
+    }
+}
