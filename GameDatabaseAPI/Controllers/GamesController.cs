240c2740d0b110fc1c88c8982c37f8d206266653
Gavin Stark <gavin@gstark.com>
2020-06-04 11:56:39 -0400

    Adds the ability to CREATE a game

--- GameDatabaseAPI/Controllers/GamesController.cs
+++ GameDatabaseAPI/Controllers/GamesController.cs
@@ -59,5 +59,73 @@ namespace GameDatabaseAPI.Controllers
             // If we do find it, return the game itself.
             return Ok(game);
         }
+
+        // Create a game
+        // 1. What do we input? -- details of a new game
+        // 2. What is the format? -- JSON in the *BODY* of the request
+        // 3. What is the name?  POST /Games
+        // 4. What is the result? The new game saved in the database
+        // 5. What is the format? JSON + Status
+        //
+        // BODY: JSON structure like
+        //       { 
+        //         "name": "Ticket To Ride",
+        //         "host": "Bill",
+        //         "address": "123 Main Street",
+        //         "when": "2020-12-25T20:05",
+        //         "minimumPlayers": 2,
+        //         "maximumPlayers": 6
+        //       } 
+        //
+        [HttpPost]
+        //     Response
+        //     |                         Deserialized from the client BODY of the *REQUEST*
+        //     |                         |
+        //     v                         v
+        public ActionResult<Game> Create(Game gameToCreate)
+        {
+            if (gameToCreate.MinimumPlayers < 2)
+            {
+                // If the min # of players < 2, error
+                var errorMessage = new
+                {
+                    message = $"You only requested {gameToCreate.MinimumPlayers} but we need at least 2."
+                };
+
+                return UnprocessableEntity(errorMessage);
+            }
+
+            if (gameToCreate.MaximumPlayers > 20)
+            {
+                // If the max # of players > 20, error
+                var errorMessage = new { message = $"You requested {gameToCreate.MaximumPlayers} but we can only support up to 20" };
+
+                return UnprocessableEntity(errorMessage);
+            }
+
+            if (gameToCreate.MaximumPlayers < gameToCreate.MinimumPlayers)
+            {
+                // If the maximum players < minimum # players, error
+
+                // Make a generic object containing just a message property
+                var errorMessage = new { message = $"Ooops, you specified a maximum number of players that is less than the minimum number" };
+
+                // Return a 422 error and supply a nice message
+                return UnprocessableEntity(errorMessage);
+            }
+
+            // Take this new game object and add it to the database
+            _context.Games.Add(gameToCreate);
+            _context.SaveChanges();
+
+            // return the game object
+            //
+            //     Generate a 201
+            //     |
+            //     |                           The game we created
+            //     |                           |
+            //     v                           v
+            return CreatedAtAction(null, null, gameToCreate);
+        }
     }
 }
