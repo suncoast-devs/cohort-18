32bda9dc4ef51bf03ac1da0dd0f1869e912852ba
Gavin Stark <gavin@gstark.com>
2020-06-22 12:00:42 -0400

    Uses our existing NewsArticle component

--- intro-to-react/src/App.jsx
+++ intro-to-react/src/App.jsx
@@ -1,21 +1,12 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+import { NewsArticle } from './components/NewsArticle'
 
 class App extends Component {
   render() {
     return (
       <div className="all-main-content">
         <main>
-          <article className="intro-article">
-            <h2 className="article-title">SDG Announces Hackathon!</h2>
-            <p>
-              SDG announces the 2020 Summer Hackathon. Join us for an exciting
-              weekend
-            </p>
-            <a className="read-more" href="#here">
-              read more about SDG Announces Hackathon!
-            </a>
-          </article>
+          <NewsArticle />
           <article className="intro-article">
             <h2 className="article-title">
               Student Graduation is Right Around the Corner
