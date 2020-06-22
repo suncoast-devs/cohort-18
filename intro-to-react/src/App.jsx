ad56e782f4e5ff7e6e8dc2d8ec555c407f31f821
Gavin Stark <gavin@gstark.com>
2020-06-22 12:09:36 -0400

    Makes a NewArticle component that is reusable with different title and body

--- intro-to-react/src/App.jsx
+++ intro-to-react/src/App.jsx
@@ -6,19 +6,19 @@ class App extends Component {
     return (
       <div className="all-main-content">
         <main>
-          <NewsArticle />
-          <article className="intro-article">
-            <h2 className="article-title">
-              Student Graduation is Right Around the Corner
-            </h2>
-            <p>
-              Our next cohort of students will be graduating in just over a
-              week.
-            </p>
-            <a className="read-more" href="#here">
-              read more about Student Graduation is Right Around the Corner
-            </a>
-          </article>
+          <NewsArticle
+            title="SDG Announces Hackathon!"
+            body="SDG announces the 2020 Summer Hackathon. Join us for an exciting weekend"
+          />
+          <NewsArticle
+            title="Student Graduation is Right Around the Corner"
+            body="Our next cohort of students will be graduating in just over a week."
+          />
+          <NewsArticle
+            title="SDG Standardizes on React"
+            body="React is the best library for learning front end Web"
+          />
+          <NewsArticle title="It is lunch time!" body="... and I'm hungry" />
         </main>
       </div>
     )
--- intro-to-react/src/components/NewsArticle.jsx
+++ intro-to-react/src/components/NewsArticle.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 
 export class NewsArticle extends React.Component {
+  // name = 'Riley'
+
+  // props --- props is an object that React.Component gives us
+  //           that contains an object with all the properties we
+  //           were called with
   render() {
     return (
       <article className="intro-article">
-        <h2 className="article-title">SDG Announces Hackathon!</h2>
-        <p>
-          SDG announces the 2020 Summer Hackathon. Join us for an exciting
-          weekend
-        </p>
+        <h2 className="article-title">{this.props.title}</h2>
+        <p>{this.props.body}</p>
         <a className="read-more" href="#here">
-          read more about SDG Announces Hackathon!
+          read more about {this.props.title}
         </a>
       </article>
     )
