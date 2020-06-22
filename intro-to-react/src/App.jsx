5f866a6c7d720a539388983a4b0e2770ac9a4864
Gavin Stark <gavin@gstark.com>
2020-06-22 16:08:38 -0400

    Rendering our articles dynamically from data we load from a JSON file

--- intro-to-react/src/App.jsx
+++ intro-to-react/src/App.jsx
@@ -3,13 +3,30 @@ import { NewsArticle } from './components/NewsArticle'
 import articles from './articles'
 
 class App extends Component {
+  convertArticleObjectToComponent(article) {
+    return (
+      <NewsArticle key={article.id} title={article.title} body={article.body} />
+    )
+  }
+
   render() {
+    const articlesWeCanPublish = articles.filter(
+      article => article.draft === false
+    )
+
     // map each object in the articles array into a NewsArticle Component
     // assign the title to that article's title and the body to that article's body
-    const newsArticlesFromData = articles.map(article => (
-      <NewsArticle title={article.title} body={article.body} />
-    ))
+    const newsArticlesFromData = articlesWeCanPublish.map(
+      this.convertArticleObjectToComponent
+    )
 
+    // newsArticlesFromData is an array (because of map)
+    //
+    // But it is an array of <NewsArticle/> components
+    //
+    // React is just a-ok with us dumping that right into the
+    // JSX/HTML. No need to join or convert, or parse.
+    //
     return (
       <div className="all-main-content">
         <main>{newsArticlesFromData}</main>
