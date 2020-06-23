53b0a39c9214db42a8b0169ce98355d0399362a5
Gavin Stark <gavin@gstark.com>
2020-06-23 11:07:51 -0400

    Initial non-dynamic JSX content and CSS

--- react-scoreboard/src/App.jsx
+++ react-scoreboard/src/App.jsx
@@ -3,7 +3,40 @@ import HelloWorld from './components/HelloWorld'
 
 class App extends Component {
   render() {
-    return <HelloWorld />
+    return (
+      <>
+        <header>
+          <h1>My Score Board</h1>
+        </header>
+        <main>
+          <section className="team1">
+            <h2>Team 1</h2>
+            <h3>0</h3>
+            <fieldset>
+              <input type="text" placeholder="Name" />
+            </fieldset>
+
+            <fieldset>
+              <i className="add fas fa-2x fa-plus-circle"></i>
+              <i className="subtract fas fa-2x fa-minus-circle"></i>
+            </fieldset>
+          </section>
+
+          <section className="team2">
+            <h2>Team 2</h2>
+            <h3>0</h3>
+            <fieldset>
+              <input type="text" placeholder="Name" />
+            </fieldset>
+
+            <fieldset>
+              <i className="add fas fa-2x fa-plus-circle"></i>
+              <i className="subtract fas fa-2x fa-minus-circle"></i>
+            </fieldset>
+          </section>
+        </main>
+      </>
+    )
   }
 }
 
