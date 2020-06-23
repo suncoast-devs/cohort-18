4acebc79a7990a4f2fce4c86186ed01a53ecd145
Gavin Stark <gavin@gstark.com>
2020-06-23 11:23:22 -0400

    Adds a scoreboard component and provides a teamName property

--- react-scoreboard/src/App.jsx
+++ react-scoreboard/src/App.jsx
@@ -1,5 +1,31 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+
+// // If this where a component
+// function greet(name) {
+//   // This would this.props.name
+//   //
+//   // Because properties are *similar to* arguments
+//   console.log(name)
+// }
+
+class Scoreboard extends Component {
+  render() {
+    return (
+      <section className="team1">
+        <h2>{this.props.teamName}</h2>
+        <h3>0</h3>
+        <fieldset>
+          <input type="text" placeholder="Name" />
+        </fieldset>
+
+        <fieldset>
+          <i className="add fas fa-2x fa-plus-circle"></i>
+          <i className="subtract fas fa-2x fa-minus-circle"></i>
+        </fieldset>
+      </section>
+    )
+  }
+}
 
 class App extends Component {
   render() {
@@ -9,31 +35,9 @@ class App extends Component {
           <h1>My Score Board</h1>
         </header>
         <main>
-          <section className="team1">
-            <h2>Team 1</h2>
-            <h3>0</h3>
-            <fieldset>
-              <input type="text" placeholder="Name" />
-            </fieldset>
-
-            <fieldset>
-              <i className="add fas fa-2x fa-plus-circle"></i>
-              <i className="subtract fas fa-2x fa-minus-circle"></i>
-            </fieldset>
-          </section>
-
-          <section className="team2">
-            <h2>Team 2</h2>
-            <h3>0</h3>
-            <fieldset>
-              <input type="text" placeholder="Name" />
-            </fieldset>
-
-            <fieldset>
-              <i className="add fas fa-2x fa-plus-circle"></i>
-              <i className="subtract fas fa-2x fa-minus-circle"></i>
-            </fieldset>
-          </section>
+          <Scoreboard teamName="SDG C#-ers" />
+          <Scoreboard teamName="SDG JS-ers" />
+          <Scoreboard teamName="SDG Staff" />
         </main>
       </>
     )
