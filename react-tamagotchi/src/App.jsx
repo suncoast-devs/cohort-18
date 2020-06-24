f070f8a6183fc8bf20887920af2533ffb05e50f7
Gavin Stark <gavin@gstark.com>
2020-06-24 11:07:21 -0400

    Basic styling

--- react-tamagotchi/src/App.jsx
+++ react-tamagotchi/src/App.jsx
@@ -3,7 +3,71 @@ import HelloWorld from './components/HelloWorld'
 
 class App extends Component {
   render() {
-    return <HelloWorld />
+    return (
+      <main className="container p-4">
+        <div className="jumbotron bg-info text-white-50">
+          <h1 className="display-4">Tamagotchi</h1>
+          <p className="lead">These are all my pets</p>
+        </div>
+        <ul className="list-group">
+          <li className="list-group-item">
+            Pet 1
+            <span className="hover-buttons float-right">
+              <span className="ml-2 badge badge-pill badge-primary">0</span>
+              <span className="ml-2 badge badge-pill badge-primary">0</span>
+              <button type="button" className="ml-2 close text-danger">
+                &times;
+              </button>
+            </span>
+            <p className="hover-buttons">
+              <button type="button" className="mr-2 btn btn-sm btn-primary">
+                Feed
+              </button>
+              <button type="button" className="mr-2 btn btn-sm btn-primary">
+                Scold
+              </button>
+              <button type="button" className="mr-2 btn btn-sm btn-primary">
+                Play
+              </button>
+            </p>
+          </li>
+          <li className="list-group-item">
+            Pet 2
+            <span className="hover-buttons float-right">
+              <span className="ml-2 badge badge-pill badge-primary">0</span>
+              <span className="ml-2 badge badge-pill badge-primary">0</span>
+              <button type="button" className="ml-2 close text-danger">
+                &times;
+              </button>
+            </span>
+          </li>
+          <li className="list-group-item">
+            Pet 3
+            <span className="hover-buttons float-right">
+              <span className="ml-2 badge badge-pill badge-primary">0</span>
+              <span className="ml-2 badge badge-pill badge-primary">0</span>
+              <button type="button" className="ml-2 close text-danger">
+                &times;
+              </button>
+            </span>
+          </li>
+          <li className="list-group-item">
+            Pet 4
+            <span className="hover-buttons float-right">
+              <span className="ml-2 badge badge-pill badge-primary">0</span>
+              <span className="ml-2 badge badge-pill badge-primary">0</span>
+              <button type="button" className="ml-2 close text-danger">
+                &times;
+              </button>
+            </span>
+          </li>
+          <li className="list-group-item">Pet 5</li>
+          <li className="list-group-item">Pet 6</li>
+          <li className="list-group-item">Pet 7</li>
+          <li className="list-group-item">Pet 8</li>
+        </ul>
+      </main>
+    )
   }
 }
 
