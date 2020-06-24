654395d6bda1239eb671a9d194c4eb499855dd55
Gavin Stark <gavin@gstark.com>
2020-06-24 11:22:34 -0400

    Loading our pets from a sample JSON file that looks just like the API!

--- react-tamagotchi/src/App.jsx
+++ react-tamagotchi/src/App.jsx
@@ -1,24 +1,29 @@
 import React, { Component } from 'react'
 import { Pet } from './components/Pet'
+import samplePetsArray from './data/sample-pets-api'
 
 class App extends Component {
+  state = {
+    pets: samplePetsArray,
+  }
+
   render() {
+    const petsToRender = this.state.pets.map(pet => (
+      <Pet
+        key={pet.id}
+        name={pet.name}
+        hungerLevel={pet.hungerLevel}
+        happinessLevel={pet.happinessLevel}
+      />
+    ))
+
     return (
       <main className="container p-4">
         <div className="jumbotron bg-info text-white-50">
           <h1 className="display-4">Tamagotchi</h1>
           <p className="lead">These are all my pets</p>
         </div>
-        <ul className="list-group">
-          <Pet name="Something 1" hungerLevel="0" happinessLevel={2} />
-          <Pet name="Something 2" hungerLevel="2" happinessLevel={0} />
-          <Pet name="Something 3" hungerLevel="0" happinessLevel={0} />
-          <Pet name="Something 4" hungerLevel="4" happinessLevel={1} />
-          <Pet name="Something 5" hungerLevel="3" happinessLevel={0} />
-          <Pet name="Something 6" hungerLevel="0" happinessLevel={0} />
-          <Pet name="Something 7" hungerLevel="0" happinessLevel={8} />
-          <Pet name="Something 8" hungerLevel="5" happinessLevel={0} />
-        </ul>
+        <ul className="list-group">{petsToRender}</ul>
       </main>
     )
   }
