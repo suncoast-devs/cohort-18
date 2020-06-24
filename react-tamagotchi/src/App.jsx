6d746316f70fa7e606327b670b52305e6d8785a6
Gavin Stark <gavin@gstark.com>
2020-06-24 11:09:18 -0400

    Extracted a Pet component

--- react-tamagotchi/src/App.jsx
+++ react-tamagotchi/src/App.jsx
@@ -1,5 +1,32 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+
+class Pet extends Component {
+  render() {
+    return (
+      <li className="list-group-item">
+        Pet 1
+        <span className="hover-buttons float-right">
+          <span className="ml-2 badge badge-pill badge-primary">0</span>
+          <span className="ml-2 badge badge-pill badge-primary">0</span>
+          <button type="button" className="ml-2 close text-danger">
+            &times;
+          </button>
+        </span>
+        <p className="hover-buttons">
+          <button type="button" className="mr-2 btn btn-sm btn-primary">
+            Feed
+          </button>
+          <button type="button" className="mr-2 btn btn-sm btn-primary">
+            Scold
+          </button>
+          <button type="button" className="mr-2 btn btn-sm btn-primary">
+            Play
+          </button>
+        </p>
+      </li>
+    )
+  }
+}
 
 class App extends Component {
   render() {
@@ -10,61 +37,17 @@ class App extends Component {
           <p className="lead">These are all my pets</p>
         </div>
         <ul className="list-group">
-          <li className="list-group-item">
-            Pet 1
-            <span className="hover-buttons float-right">
-              <span className="ml-2 badge badge-pill badge-primary">0</span>
-              <span className="ml-2 badge badge-pill badge-primary">0</span>
-              <button type="button" className="ml-2 close text-danger">
-                &times;
-              </button>
-            </span>
-            <p className="hover-buttons">
-              <button type="button" className="mr-2 btn btn-sm btn-primary">
-                Feed
-              </button>
-              <button type="button" className="mr-2 btn btn-sm btn-primary">
-                Scold
-              </button>
-              <button type="button" className="mr-2 btn btn-sm btn-primary">
-                Play
-              </button>
-            </p>
-          </li>
-          <li className="list-group-item">
-            Pet 2
-            <span className="hover-buttons float-right">
-              <span className="ml-2 badge badge-pill badge-primary">0</span>
-              <span className="ml-2 badge badge-pill badge-primary">0</span>
-              <button type="button" className="ml-2 close text-danger">
-                &times;
-              </button>
-            </span>
-          </li>
-          <li className="list-group-item">
-            Pet 3
-            <span className="hover-buttons float-right">
-              <span className="ml-2 badge badge-pill badge-primary">0</span>
-              <span className="ml-2 badge badge-pill badge-primary">0</span>
-              <button type="button" className="ml-2 close text-danger">
-                &times;
-              </button>
-            </span>
-          </li>
-          <li className="list-group-item">
-            Pet 4
-            <span className="hover-buttons float-right">
-              <span className="ml-2 badge badge-pill badge-primary">0</span>
-              <span className="ml-2 badge badge-pill badge-primary">0</span>
-              <button type="button" className="ml-2 close text-danger">
-                &times;
-              </button>
-            </span>
-          </li>
-          <li className="list-group-item">Pet 5</li>
-          <li className="list-group-item">Pet 6</li>
-          <li className="list-group-item">Pet 7</li>
-          <li className="list-group-item">Pet 8</li>
+          <Pet />
+          <Pet />
+          <Pet />
+          <Pet />
+          <Pet />
+          <Pet />
+          <Pet />
+          <Pet />
+          <Pet />
+          <Pet />
+          <Pet />
         </ul>
       </main>
     )
