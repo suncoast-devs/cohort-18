2751756f2012ec2cbb002e96661280acdb0b7df5
Gavin Stark <gavin@gstark.com>
2020-06-25 11:25:07 -0400

    ShowPet renders from state
    But the state is hard coded

--- react-tamagotchi-with-router/src/components/ShowPet.jsx
+++ react-tamagotchi-with-router/src/components/ShowPet.jsx
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
 
 export class ShowPet extends Component {
-  render() {
-    console.log(this.props)
+  state = {
+    pet: {
+      id: 0,
+      name: 'string',
+      birthday: '2020-06-25T14:58:05.208Z',
+      deathday: '2020-06-25T14:58:05.208Z',
+      hungerLevel: 0,
+      happinessLevel: 0,
+      isDead: true,
+      lastInteracted: '2020-06-25T14:58:05.208Z',
+    },
+  }
 
+  render() {
     // Uses the properties passed to us to
     // get at the :petid part of
     //
@@ -13,12 +24,12 @@ export class ShowPet extends Component {
     return (
       <ul className="list-group">
         <li className="list-group-item">{petId}</li>
-        <li className="list-group-item">Pet Name</li>
-        <li className="list-group-item">Birthday</li>
-        <li className="list-group-item">Deathday</li>
-        <li className="list-group-item">Happiness</li>
-        <li className="list-group-item">HungerLevel</li>
-        <li className="list-group-item">Last Interacted</li>
+        <li className="list-group-item">{this.state.pet.name}</li>
+        <li className="list-group-item">{this.state.pet.birthday}</li>
+        <li className="list-group-item">{this.state.pet.deathday}</li>
+        <li className="list-group-item">{this.state.pet.happinessLevel}</li>
+        <li className="list-group-item">{this.state.pet.hungerLevel}</li>
+        <li className="list-group-item">{this.state.pet.lastInteracted}</li>
       </ul>
     )
   }
