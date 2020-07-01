6022be6a93ee60cf4cadefd3757a1a4378061aa8
Gavin Stark <gavin@gstark.com>
2020-07-01 11:25:08 -0400

    Can load photo from api by using *useEffect*

--- astronomy-photo-of-the-day/src/App.jsx
+++ astronomy-photo-of-the-day/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import HelloWorld from './components/HelloWorld'
 
 function App() {
@@ -9,6 +9,42 @@ function App() {
   // if photoDetails.copyright is "TRUTHY" (in this case anything other than undefined)
   // make a string with a byline text, otherwise make it blank
   const byLine = photoDetails.copyright ? `by ${photoDetails.copyright}` : ''
+
+  const loadPhotoFromApi = () => {
+    const url =
+      'https://apodapi.herokuapp.com/api?image_thumbnail_size=300&absolute_thumbnail_url=true&thumbs=true'
+
+    // Fetch the URL (get)
+    fetch(url)
+      // Then we turn the response into JSON
+      .then((response) => response.json())
+      // Then we have the API data as an object
+      .then((apiData) => {
+        setPhotoDetails(apiData)
+      })
+  }
+
+  // Call loadPhotoFromApi the first
+  // time our component renders.
+  //
+  // Then watch the array for any
+  // changing values and if any
+  // values array, change, call
+  // loadPhotoFrom Api
+  //
+  // ... but the array is empty
+  // and thus will never change
+  // so we'll never see loadPhotoFromApi
+  // called again.
+  useEffect(loadPhotoFromApi, [])
+
+  // useEffect(function, arrayToWatch)
+  // The function is the work to do
+  //
+  // The array is a set of values for
+  // useEffect to monitor. If any of
+  // those values *CHANGE* the function
+  // is called!
 
   return (
     <div className="container">
