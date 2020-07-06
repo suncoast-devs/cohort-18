45fe36b1779ff6ac72e2044214b182fc60c021f5
Gavin Stark <gavin@gstark.com>
2020-07-06 11:55:54 -0400

    Added fetch to retrieve list of restaurants from API we just built

--- TacoTuesday/ClientApp/src/pages/Restaurants.jsx
+++ TacoTuesday/ClientApp/src/pages/Restaurants.jsx
@@ -1,7 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
+function SingleRestaurantForList(props) {
+  return (
+    <Link
+      to="/restaurants/42"
+      className="list-group-item list-group-item-action"
+    >
+      <div className="d-flex w-100 justify-content-between">
+        <h5 className="mb-1">{props.restaurant.name}</h5>
+        <small>5 Reviews</small>
+      </div>
+      <p className="mb-1">{props.restaurant.address}</p>
+      <small className="mr-3">
+        <button className="btn btn-success btn-sm">
+          <span className="mr-2" role="img" aria-label="upvote">
+            👍🏻
+          </span>
+          5
+        </button>
+      </small>
+      <small className="mr-3">
+        <button className="btn btn-danger btn-sm">
+          <span className="mr-2" role="img" aria-label="downvote">
+            👎🏻
+          </span>{' '}
+          3
+        </button>
+      </small>
+    </Link>
+  )
+}
+
 export function Restaurants() {
+  const [restaurants, setRestaurants] = useState([])
+
+  useEffect(() => {
+    // code to do when the dependencies change
+    fetch('/api/Restaurants')
+      .then(response => response.json())
+      .then(apiData => {
+        // Finally have data from the API
+        console.log('The API said....')
+        console.log(apiData)
+        setRestaurants(apiData)
+      })
+  }, []) // <== [] is the list of dependencies, right now there are none, so only runs code at start
+
   return (
     <>
       <nav aria-label="breadcrumb">
@@ -10,64 +55,17 @@ export function Restaurants() {
             <Link to="#">Home</Link>
           </li>
           <li className="breadcrumb-item active" aria-current="page">
-            2 Taco Joints Found
+            {restaurants.length} Taco Joints Found
           </li>
         </ol>
       </nav>
       <div className="list-group">
-        <Link
-          to="/restaurants/42"
-          className="list-group-item list-group-item-action"
-        >
-          <div className="d-flex w-100 justify-content-between">
-            <h5 className="mb-1">Taco Heaven</h5>
-            <small>5 Reviews</small>
-          </div>
-          <p className="mb-1">123 Main Street</p>
-          <small className="mr-3">
-            <button className="btn btn-success btn-sm">
-              <span className="mr-2" role="img" aria-label="upvote">
-                👍🏻
-              </span>
-              5
-            </button>
-          </small>
-          <small className="mr-3">
-            <button className="btn btn-danger btn-sm">
-              <span className="mr-2" role="img" aria-label="downvote">
-                👎🏻
-              </span>{' '}
-              3
-            </button>
-          </small>
-        </Link>
-
-        <Link
-          to="/restaurants/42"
-          className="list-group-item list-group-item-action"
-        >
-          <div className="d-flex w-100 justify-content-between">
-            <h5 className="mb-1">Taco Mania</h5>
-            <small>2 Reviews</small>
-          </div>
-          <p className="mb-1">555 East Pima</p>
-          <small className="mr-3">
-            <button className="btn btn-success btn-sm">
-              <span className="mr-2" role="img" aria-label="upvote">
-                👍🏻
-              </span>
-              10
-            </button>
-          </small>
-          <small className="mr-3">
-            <button className="btn btn-danger btn-sm">
-              <span className="mr-2" role="img" aria-label="downvote">
-                👎🏻
-              </span>{' '}
-              1
-            </button>
-          </small>
-        </Link>
+        {restaurants.map(restaurant => (
+          <SingleRestaurantForList
+            key={restaurant.id}
+            restaurant={restaurant}
+          />
+        ))}
       </div>
     </>
   )
--- TacoTuesday/Models/seeds.sql
+++ TacoTuesday/Models/seeds.sql
@@ -6,3 +6,4 @@ INSERT INTO "Restaurants" ("Name", "Description", "Address", "Telephone") VALUES
 INSERT INTO "Restaurants" ("Name", "Description", "Address", "Telephone") VALUES ('Dabtype', 'Organized stable firmware', '7 Miller Park', '523-760-6681');
 INSERT INTO "Restaurants" ("Name", "Description", "Address", "Telephone") VALUES ('Topdrive', 'Object-based interactive application', '65 Eliot Lane', '650-993-7074');
 INSERT INTO "Restaurants" ("Name", "Description", "Address", "Telephone") VALUES ('Avaveo', 'Persistent zero defect process improvement', '2 Clarendon Junction', '715-663-5265');
+INSERT INTO "Restaurants" ("Name", "Description", "Address", "Telephone") VALUES ('Gavins Place', 'Best in Town', '2020 Central Ave', '727-555-1212');
