193e3c2d569424739507e77280d340bf9bbb84d6
Gavin Stark <gavin@gstark.com>
2020-07-07 10:49:47 -0400

    Adds some tricky code to the AddRestaurant that tracks an object that matches what the API is expecting for a new restaurant

--- TacoTuesday/ClientApp/src/pages/AddRestaurant.jsx
+++ TacoTuesday/ClientApp/src/pages/AddRestaurant.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export function AddRestaurant() {
+  const [newRestaurant, setNewRestaurant] = useState({
+    name: '',
+    description: '',
+    address: '',
+    telephone: '',
+  })
+
+  const handleFormFieldChange = event => {
+    const whichFieldChanged = event.target.id
+    const newValue = event.target.value
+
+    console.log(`The ${whichFieldChanged} was changed`)
+    setNewRestaurant({
+      ...newRestaurant,
+      // The [] syntax used where the key of an object exists
+      // means to take the value of the variable INSIDE the brackets
+      // as the name of the property we are setting.
+      [whichFieldChanged]: newValue,
+    })
+  }
+
   return (
     <div className="card">
       <div className="card-header">Add a Restaurant</div>
@@ -8,25 +29,49 @@ export function AddRestaurant() {
         <form>
           <div className="form-group">
             <label htmlFor="name">Name</label>
-            <input type="text" className="form-control" id="name" />
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              value={newRestaurant.name}
+              onChange={handleFormFieldChange}
+            />
           </div>
 
           <div className="form-group">
             <label htmlFor="description">Description</label>
-            <textarea type="text" className="form-control" id="description" />
+            <textarea
+              type="text"
+              className="form-control"
+              id="description"
+              value={newRestaurant.description}
+              onChange={handleFormFieldChange}
+            />
             <small id="descriptionHelp" className="form-text text-muted">
               Enter a brief description of the restaurant.
             </small>
           </div>
 
           <div className="form-group">
-            <label htmlFor="description">Address</label>
-            <textarea type="text" className="form-control" id="address" />
+            <label htmlFor="address">Address</label>
+            <textarea
+              type="text"
+              className="form-control"
+              id="address"
+              value={newRestaurant.address}
+              onChange={handleFormFieldChange}
+            />
           </div>
 
           <div className="form-group">
             <label htmlFor="telephone">Telephone</label>
-            <input type="text" className="form-control" id="telephone" />
+            <input
+              type="text"
+              className="form-control"
+              id="telephone"
+              value={newRestaurant.telephone}
+              onChange={handleFormFieldChange}
+            />
           </div>
 
           <button type="submit" className="btn btn-primary">
